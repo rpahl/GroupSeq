761efa9cfed9201ea9db46b110ec0e5ee652c5c4
Roman Pahl <roman.x.pahl@gsk.com>
2018-04-11 15:20:55 +0200

    Remove break-statements in computeAlphaLevel and revise this and stdDeviations

--- R/computeAlphaLevel.R
+++ R/computeAlphaLevel.R
@@ -1,135 +1,64 @@
 "computeAlphaLevel" <-
 function(n, t, t2, lowerBounds, upperBounds, drift, nMax)
 {
-  ######################
-  #INITIALIZE VARIABLES#
-  ######################
-  probStopping <-0 #is the probaility of reaching ith analysis and stopping.
-  probExceedingUpper<-0 # the probaility of reaching ith and exceeding upper.
-  probExceedingLower<-0 #is the probability of reaching ith and exceeding lower.
-  expectedStoppingTime<-0 # not implemented yet
-  probTotal <-0 #is the total probability of rejecting, sum(1,n)(probExceedingUpper+probExceedingLower)
-  gridSize<-0.05 # the grid size for numerical integration by trapezoidal rule
-  lastGrid<-0 # the grid of the joint density at the last analysis.
-  maxDimForLastGrid<-5000 # the maximum dimension for lastGrid().
-  numberOfIntegrationIntervalls<-0 #"integer" the number of intervals for numerical integration
-  lowerIntegrationLimit<-0 # the vector of lower integration limits.
-  upperIntegrationLimit<-0 # the vector of upper integration limits.
-  standardDeviation<-0 #  the standard deviation of the process increment
-  standardDeviationProcess<-0 #the standard deviation of the process.
-  noError<-TRUE
+    # Initialize
+    p.stop <-0 #is the probaility of reaching ith analysis and stopping.
+    gridSize <- 0.05 # the grid size for numerical integration by trapezoidal rule
+    gridVec <- 0 # the grid vector of the most recent joint density calculation
+    gridVec.maxLen <- 5000 # the maximum dimension for gridVec
 
+    if(n > nMax) {
+        stop(" Number of analyses too large - got ", n, " but allowed are at most ", nMax)
+    }
 
-  ##Check limit for number of interim analyses (looks).
-  if(n>nMax)
-  {
-    cat(" Number of analyses too large - you got ",n,"\n")
-    cat(" but allowed are at most ",nMax,"\n")
-    cat("change parameter 'nMax' in function 'input.R' for a larger number.","\n")
-    break ##stop calculating
-  }
+    # Dtandard deviations of increments and process
+    sd.inc <- c(sqrt(t2[1]), sqrt(diff(t2)))
+    sd.proc <- sqrt(t2)
 
-  ###--- Calculate standard deviations of increments and process ---###
-  ##function stdDeviations(...) returns (standardDeviation, standardDeviationProcess)
-  stdDeviationVector<-stdDeviations( n, t2 )
-  standardDeviation <- stdDeviationVector[[1]]
-  standardDeviationProcess <- stdDeviationVector[[2]]
+    # Normalized upper and lower integration limits
+    upper <- (upperBounds*sd.proc) - (drift*t)
+    lower <- (lowerBounds*sd.proc) - (drift*t)
 
-  ##Set upper (upperIntegrationLimit) and lower (lowerIntegrationLimit) integration limits.
+    # Number of intervals for numerical integration
+    gridLen <- abs(trunc((upper-lower) / (gridSize*sd.inc)) + 1)
 
-  #upper integration limits
-  upperIntegrationLimit <- (upperBounds*standardDeviationProcess)-(drift*t)
+    if (any(gridLen > gridVec.maxLen)) {
+        stop("Too many grid points - got ", max(gridLen, na.rm=TRUE),
+             " but allowed are at most ", gridVec.maxLen)
+    }
 
-  #lower integration limits
-  lowerIntegrationLimit <- (lowerBounds*standardDeviationProcess)-(drift*t)
+    # Calculate probabilities
+    # -----------------------
+    # One-dimensional probs at t[1] are calculated using standard stats::pnorm
+    ndrift <- drift*t[1]/sd.inc[1]  # normalized drift
+    p.stop <- 1 - (pnorm(upperBounds[1] - ndrift) - pnorm(lowerBounds[1] - ndrift))
+    p.exit.upper <- 1 - pnorm(upperBounds[1] - ndrift)
+    p.exit.lower <- pnorm(lowerBounds[1] - ndrift)
 
-  #number of intervals for numerical integration
-  #numberOfIntegrationIntervalls is a vector of length n!
-  numberOfIntegrationIntervalls <- abs( trunc( (upperIntegrationLimit-lowerIntegrationLimit) / (gridSize*standardDeviation) ) + 1)
+    # Multidimensional probabilities require numerical integration
+    if(n>=2) {
+        for (i in 2:n) {
+            if(i==2) {
+                # Density of process at first look
+                gridVec <- jointDensity(1, lower[1], upper[1], sd.inc[1], gridLen, gridVec)
+            }
 
-  ###Check limit for number of grid points.
-  if( any(numberOfIntegrationIntervalls>maxDimForLastGrid) )
-  {
-    cat(" Error in function 'computeAlphaLevel'","\n")
-    cat(" Bounds too wide - namely (b-a)/gridSize is greater than ",maxDimForLastGrid,"\n")
-    break ##stop calculating
-  }
+            res <- computeCurrentProbability(gridVec, gridLen, lower, upper,i,gridSize, sd.inc[i])
+            p.stop[i] <- res[[1]]
+            p.exit.upper[i] <- res[[2]]
+            p.exit.lower[i] <- res[[3]]
+            p.stop[i] <- 1 - p.stop[i]
 
+            # If density needed for next step, compute it with parameter i
+            if (i != n) {
+                gridVec <- jointDensity(i, lower, upper, sd.inc[i], gridLen, gridVec)
+            }
+        }
+    }
 
-  ##----------------------------------------------------##
-  #- Begin loop calculating probabilities for each look -#
-  #------------------------------------------------------#
-  #- For first look some direct calculation is possible -#
-  #--- using normal cdf.  Bounds in standard form are ---#
-  #-  adjusted for mean of process at t(1). -------------#
-  ##----------------------------------------------------##
+    p.total <- sum(p.exit.upper) + sum(p.exit.lower)
 
-
-  ##probability of reaching ith analysis and stopping.
-  probStopping[1] <- 1 - ( pnorm( upperBounds[1]-drift*t[1]/standardDeviation[1] )
-                  -pnorm( lowerBounds[1]-drift*t[1]/standardDeviation[1] ) )
-  ##probability of reaching ith and exceeding upper.
-  probExceedingUpper[1] <- 1 - pnorm( upperBounds[1]-drift*t[1]/standardDeviation[1] )
-
-  ##probability of reaching ith and exceeding lower
-  probExceedingLower[1] <- pnorm( lowerBounds[1]-drift*t[1]/standardDeviation[1] )
-
-
-  ##Accumulate boundary crossing probability.
-  probTotal <- probTotal + ( probExceedingUpper[1]+probExceedingLower[1] )
-
-  if(n>=2)
-  {
-    ##After the first look, numerical integration is necessary.
-    for (i in 2:n)
-    {
-      ##-----------------------------------------##
-      # Compute density of process at first look. #
-      ##-----------------------------------------##
-
-      if(i==2)
-      {
-        ##call function 'jointDensity' with parameter==1,
-        ##for computing joint Density in 1st analysis - look also function jointDensity
-        lastGrid <- jointDensity(1, lowerIntegrationLimit[1], upperIntegrationLimit[1], standardDeviation[1],numberOfIntegrationIntervalls,lastGrid)
-        #                    |
-        #               parameter==1
-      }
-
-      ##----------------------------------------------------------------------##
-      ##--Compute probStopping, probExceedingUpper and probExceedingLower in function computeCurrentProbability.--##
-      ##----------------------------------------------------------------------##
-      vectorOfResults <-       computeCurrentProbability(lastGrid,numberOfIntegrationIntervalls,lowerIntegrationLimit,upperIntegrationLimit,i,gridSize,
-      standardDeviation[i])
-      probStopping[i] <- vectorOfResults[[1]]
-      probExceedingUpper[i] <- vectorOfResults[[2]]
-      probExceedingLower[i] <- vectorOfResults[[3]]
-
-      probStopping[i] <- 1 - probStopping[i]
-
-      ##If density will be needed for next step, compute it with parameter i
-      if(i!=n)
-      {
-        lastGrid <- jointDensity(i, lowerIntegrationLimit, upperIntegrationLimit, standardDeviation[i], numberOfIntegrationIntervalls, lastGrid)
-      }
-
-      ##Accumulate boundary crossing probability.
-      probTotal <- probTotal + ( probExceedingUpper[i]+probExceedingLower[i] )
-
-
-      ## Expected stopping time.  (Not implemented.)
-      ## expectedStoppingTime <- expectedStoppingTime + (probExceedingUpper[i]+probExceedingLower[i])*(1.d0-t [i]) ##
-
-    }#end <--*for (i in 2:n)*
-  }#end <--*if(n>=2)*
-
-  ## Compute expected stopping time.  (Not implemented.)
-  ## expectedStoppingTime = 1 - expectedStoppingTime
-
-  ##Return the values ('probStopping' and 'expectedStoppingTime' are always equal 0 yet, cause of still missing implementations
-  toBeReturned<-list(probAndStop=probStopping, probAndExceedingUpper=probExceedingUpper, probAndExceedingLower=probExceedingLower,
-  expectedStoppingTime=expectedStoppingTime,totalTypeOneError=probTotal)
-  return(toBeReturned)
-
-
-}#end <--*function(...)*
+    list(probAndStop=p.stop, probAndExceedingUpper=p.exit.upper,
+         probAndExceedingLower=p.exit.lower, expectedStoppingTime=0,
+         totalTypeOneError=p.total)
+}
