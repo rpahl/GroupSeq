a7cda1b610b6b2c7ca1d1c117555d7fc29c72e87
Roman Pahl <roman.pahl@gmail.com>
2018-12-25 17:51:29 +0100

    Update description to upcoming new version; switch to roxygen documenation

--- R/groupseq.R
+++ R/groupseq.R
@@ -1,9 +1,28 @@
+#' @title Group sequential design probabilities - with graphical user interface.
+#'
+#' @description Computes probabilities related to group sequential designs for
+#'  normally distributed test statistics. Enables to derive critical
+#'  boundaries, power, drift, and confidence intervals of such designs.
+#'  Supports the alpha spending approach by Lan-DeMets as well as the
+#'  conditional rejection probability principle by Müller and Schäfer.
+#'
+#' @author Roman Pahl, \email{roman.pahl@gmail.com}
+#' @docType package
+#' @name groupseq.pkg
+NULL
+
+
 pkg.env <- new.env(parent=emptyenv())
 pkg.env$taskWindow <- NULL
 pkg.env$scipen.old <- options(scipen=10)[[1]]
 
 
-groupseq <- function(mode="g")
+#' @title Start GroupSeq
+#' @description Starts the graphical user interface, optionally the legacy
+#'  version of GroupSeq prior to version 2.0.
+#' @param legacy (logical) if \code{TRUE} starts legacy GroupSeq version < 2.0
+#' @export
+groupseq <- function(legacy=FALSE)
 {
     mode <- match.arg(mode)
     switch(mode,
@@ -11,11 +30,15 @@ groupseq <- function(mode="g")
            stop("Invalid mode '", mode, "'"))
 }
 
+
+
+#' @keywords internal
 .onAttach <- function(libname, pkgname)
 {
-    groupseq()
+    #groupseq()
 }
 
+#' @keywords internal
 quitGroupSeq <- function() {
     if (!is.null(pkg.env$taskWindow)) {
         tcltk::tkdestroy(pkg.env$taskWindow)
@@ -25,11 +48,13 @@ quitGroupSeq <- function() {
     invisible()
 }
 
+#' @keywords internal
 .onUnload <- function(libpath)
 {
     quitGroupSeq()
 }
 
+#' @keywords internal
 .onDetach <- function(libpath)
 {
     quitGroupSeq()
