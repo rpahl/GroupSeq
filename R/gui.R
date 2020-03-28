344a78c2cd08e99556153e243a0c194f5e8856da
Roman Pahl <roman.pahl@gmail.com>
2020-03-28 20:04:54 +0100

    Add menubar and custom icons

--- R/gui.R
+++ R/gui.R
@@ -1,3 +1,12 @@
+#' @keywords internal
+add_custom_icons <- function()
+{
+    path <- system.file("icons", package = "GroupSeq")
+    iconFiles <- list.files(path, full.names = TRUE, pattern = "*.png")
+    iconNames <- tools::file_path_sans_ext(basename(iconFiles))
+    addStockIcons(iconNames, iconFiles)
+}
+
 #' @title Create graphical user interface.
 #' @description This function builds the main GUI that appears when [GroupSeq]
 #' is started.
@@ -8,8 +17,19 @@ gui <- function()
 {
     version <- utils::packageVersion(utils::packageName())
     #options("guiToolkit" = "tcltk")
+    options("guiToolkit"="RGtk2")
+    add_custom_icons()
     win <- gwindow(title = paste0("GroupSeq (version ", version, ")"),
                   visible = TRUE)
+
+    items.file <- list(quit = gaction("Quit", icon = "quit",
+                                      handler = function(...) dispose(win)))
+    items.calc <- list(calc = gaction("Calculate", icon = "matrix"))
+    items.help <- list(about = gaction("About", icon = "info-icon"))
+    menubar <- list("File" = items.file,
+                    "Calculate" = items.calc,
+                    "Help" = items.help)
+    gmenu(menubar, container = win)
 
     grp_main <- ggroup(horizontal = TRUE, container = win)
 
