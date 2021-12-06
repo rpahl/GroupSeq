128d4870232e894189fb9a1ac30f8dfd3b048307
Roman Pahl <roman.pahl@gmail.com>
2021-12-06 19:37:52 +0100

    Remove verify_arg util function

--- R/utils.R
+++ R/utils.R
@@ -1,35 +1,3 @@
-#' @title Verify function arguments
-#' @description This function enables shortened arg checking.
-#' @param x object to verify
-#' @param type `character` expected class or type.
-#' @param lenOp `character` operator used for length comparison.
-#' @param len `integer` expected length of argument.
-#' @return returns invisibly `TRUE` if verification succeeds
-#' @noRd
-verify_arg <- function(x,
-                       type = "character",
-                       lenOp = c("==", ">", "<", ">=", "<=", "!="),
-                       len = 1L
-                       )
-{
-    # Arg checks
-    stopifnot(is.character(type),
-              length(len) == 1,
-              is.integer(len),
-              is.character(lenOp))
-    op <- match.arg(lenOp)
-    xstr <- deparse(substitute(x))
-
-    # Verify
-    if (!inherits(x, type)) stop(xstr, " is not of type ", type)
-    if (!do.call(op, args = list(length(x), len))) {
-        stop("length(", xstr, ") ", op, " ", len, " is not true")
-    }
-
-    invisible(TRUE)
-}
-
-
 
 update_tcl_parameters_from_list <- function(dict, param_list)
 {
